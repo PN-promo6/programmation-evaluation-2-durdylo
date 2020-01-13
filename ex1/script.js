65fe3c90a8d991b7b86325283f8ea0f6f4e250a9
Loïc Denis <loic.alber.denis@gmail.com>
2020-01-13 14:19:38 +0100

    fin ex1

--- ex1/script.js
+++ ex1/script.js
@@ -4,3 +4,35 @@ let fishlist = [
   ["Aulopiformes","Congridae","Congre","	Conger conger"],
   ["Atheriniformes","Atherinidae","Joël","	Atherina boyeri"]
 ]
+
+// creating a function to display a table in the web browser
+
+function createTableHTML(array) {
+  let i=0
+  let j=0
+  let currentElement
+  let currentElement2
+
+  let newTable = document.createElement("table");
+  document.body.appendChild(newTable);
+
+  let newTableBody = document.createElement("tbody");
+  newTable.appendChild(newTableBody);
+
+
+  while (i<array.length) {
+    currentElement =array[i]
+    let newTableRow = document.createElement("tr")
+    newTableBody.appendChild(newTableRow)
+    j=0
+    while (j<currentElement.length) {
+      currentElement2=currentElement[j]
+      let newTableData=document.createElement("td")
+      newTableData.textContent=currentElement2
+      newTableRow.appendChild(newTableData)
+      j++
+    }
+    i++
+  }
+}
+createTableHTML(fishlist)
