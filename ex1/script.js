2a0e915af7f44754412f935e5def5162c4848167
Loïc Denis <loic.alber.denis@gmail.com>
2020-01-13 14:10:17 +0100

    first commit

---
+++ ex1/script.js
@@ -0,0 +1,6 @@
+let fishlist = [
+  ["Aulopiformes","Synodontidae","Poisson lézard","Synodus saurus"],
+  ["Aulopiformes","Anguilliformes","Anguille","Anguilla anguilla"],
+  ["Aulopiformes","Congridae","Congre","	Conger conger"],
+  ["Atheriniformes","Atherinidae","Joël","	Atherina boyeri"]
+]
